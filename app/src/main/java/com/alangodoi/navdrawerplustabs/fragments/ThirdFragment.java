d04ba2857d6292694ff76394a4d19bc3cba18264
alangodoi <alangsilveira@gmail.com>
2016-07-07 14:57:48 -0300

    Added correct fragment layout

--- app/src/main/java/com/alangodoi/navdrawerplustabs/fragments/ThirdFragment.java
+++ app/src/main/java/com/alangodoi/navdrawerplustabs/fragments/ThirdFragment.java
@@ -15,6 +15,6 @@ public class ThirdFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        return inflater.inflate(R.layout.fragment_third, container, false);
     }
 }
